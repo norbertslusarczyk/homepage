7da6bd5394dd691ef97e8eee69937a08b46abfdb
Norbert Ślusarczyk <norbertslusarczyk@gmail.com>
2021-01-19 11:39:37 +0100

    initial commit

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 alert('siema');
 console.log('siema');
-const name = 'Norber';
+const name = 'Norbert';
 const age = 40;
 console.log(name);
 console.log(age);
-console.log(`CZeść  jestem ${name} i mam ${age}`); 
+console.log(`CZeść jestem ${name} i mam ${age} lat.`); 
